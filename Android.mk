963a578009d34fc7fcc3088bddde76ae906b3425
Joe Maples <joe@frap129.org>
2016-06-22 02:06:49 +0000

    Add new source file
    Signed-off-by: Joe Maples <joe@frap129.org>

--- Android.mk
+++ Android.mk
@@ -210,6 +210,7 @@ vg_local_src_files := \
 	coregrind/m_debuginfo/readmacho.c \
 	coregrind/m_debuginfo/readpdb.c \
 	coregrind/m_debuginfo/storage.c \
+        coregrind/m_debuginfo/tinfl.c \
 	coregrind/m_debuginfo/tytypes.c \
 	coregrind/m_deduppoolalloc.c \
 	coregrind/m_demangle/cp-demangle.c \
